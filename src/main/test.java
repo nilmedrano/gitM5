8026e7a3685ffa801c32b20c4a694b48f7aad1d7
nilmedrano <medranonii@gmail.com>
2023-02-13 18:33:26 +0100

    Modified content

--- src/main/test.java
+++ src/main/test.java
@@ -3,7 +3,6 @@ package main;
 public class test {
 	public static void main(String args[]) {
 		System.out.print("Hello! I'm the fourth activity.");
-		System.out.print("And now they have added this line.");
-
+		System.out.print("And now they have added this line");
 	}
 }
